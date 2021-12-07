8c0ef3ced0369a0c8c63be04d4f0e43bf0cac228
MDatsii <kit.dnm@gmail.com>
2021-12-08 00:45:15 +0200

    1

--- lab1-datsii/src/app.controller.ts
+++ lab1-datsii/src/app.controller.ts
@@ -5,19 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  /* @Get()
-  get() {
-    return this.appService.getHello()
-    + '<br/>' + this.appService.getStrings()
-    + '<br/>' + this.appService.getArrays()
-    + '<br/>' + this.appService.getNumbers()
-    + '<br/>' + this.appService.getEnum()
-    + '<br/>' + this.appService.getMultipleArrays()
-    + '<br/>' + this.appService.getMultipleValues();
-  } */
-
   @Get()
-  get() {
+  getHello(): any {
     return this.appService.getHello();
   }
 }
--- lab1-datsii/src/app.service.ts
+++ lab1-datsii/src/app.service.ts
@@ -2,57 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): any {
-    let a,b : number;
-    a = 13;
-    b = 11;
-    const res = this.addTwoNumbers(a,b);
-    
-    if (res%2==0) {
-      return res + " - парне число"
-    }
-    else return res + "- непарне число";
+  getHello(): Person {
+    const person = new Person('Дацій', 'Микола', '20', 'Червоний');
+    return person;
   }
-  
-    public addTwoNumbers(a: number, b: number): any {
-      const result = a + b;
-      return result;
-    }
+}
 
- /* getNumbers(): number {
-    const a = 90;
-    const b = 3.14;
-    const c = a + b;
-    return c;
-  }
+export class Person {
+  LastName : string;
+  FirstName : string;
+  Age : number;
+  EyeColor : string;
 
-  getStrings(): string {
-    const name = 'Mykola';
-    const lastname = ' Datsii';
-    const fullname = name + lastname;
-    return fullname;
+  constructor(LastName, FirstName, Age, EyeColor) {
+    this.LastName = LastName;
+    this.FirstName = FirstName;
+    this.Age = Age;
+    this.EyeColor = EyeColor;
   }
-
-  getArrays(): number[] {
-    const list: number[] = [9,8,7];
-    return list;
-  }
-
-  getEnum(): any {
-    enum Color {Red = 'Red', Green = 'Green', Blue = 'Blue'};
-    let c: Color = Color.Red;
-    return c;
-  }
-
-  getMultipleArrays(): number[][] {
-    const A: number[][] = [[1,2,3],[1,2,3]];
-    return A;
-  }
-
-  getMultipleValues(): [number, string] {
-    var employee: [number, string] = [1, 'Billy'];
-    employee.push(2, 'John');
-    return employee;
-  }
-*/  
 }
